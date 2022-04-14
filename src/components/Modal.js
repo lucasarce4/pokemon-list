ce289daa61b828128022cb5e7d1998a7f1d36187
Lucas Arce <lucasearce4@gmail.com>
2022-04-14 19:58:14 -0300

    Updated searchbar

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 function Modal({setShowModal,currentInfo}){
-    return <div className="w-full h-full absolute bottom-0 left-0 bg-gray-300/50">
+    return <div className="w-screen h-screen absolute bottom-0 left-0 bg-gray-300/50">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit h-fit bg-slate-300 p-20">
             <p className="capitalize">{currentInfo.name}</p>
             <button onClick={()=>setShowModal(false)}>Close</button>
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,33 +1,58 @@
-import { useState,useEffect } from 'react'
+import { useState } from 'react'
+import Modal from './Modal'
 
 function Searchbar ({pokeList}){
     const [focus,setFocus] = useState(false)
     const [searchList,setSearchList] = useState([])
+    const [pokemon,setPokemon] = useState('')
+    const [showModal,setShowModal] = useState(false)
+    
 
     const handleList = (e)=>{
+        if(e.target.value === ''){
+            setSearchList([...pokeList.slice(0,20)])
+            return
+        }
         const list = pokeList.filter(poke=>{
             return (poke.name.includes(e.target.value))
         })
         setSearchList([...list.slice(0,20)])
     }
 
-    useEffect(()=>{
-        const list = pokeList.slice(0,20)
-        setSearchList([...list])
-    },[])
+    const handleFocus = (e)=>{
+        setFocus(true)
+        handleList(e)
+    }
+    const handleClick = async (item)=>{
+        const res = await fetch(item.url)
+        const data = await res.json()
+        setPokemon(data)
+        setShowModal(true)
+    }
 
-    return <div className='relative'>
-        <input onFocus={()=>setFocus(true)} 
-        onBlur={()=>setFocus(false)}
-        onChange={handleList}
-        className="border border-slate-400" type="text" />
-        {!focus ? null :
-         <ul className='absolute bg-slate-200 w-full'>
-             {
-             searchList.map(item=>{
-                 return <li key={item.name}>{item.name}</li>
+    return <div onBlur={()=>setFocus(false)}>
+            <div className='relative'>
+
+                <input onFocus={(e)=>handleFocus(e)} 
+                onChange={handleList}
+                className="border border-slate-400" type="text" />
+                {!focus ? null :
+                 <ul className='absolute bg-slate-200 w-full'>
+                     {
+                         searchList.map(item=>{
+                         return <div
+                            key={item.name} 
+                            className="border border-gray-400 py-1 px-2 cursor-pointer hover:bg-slate-300"
+                            onClick={()=>handleClick(item)}
+                            onMouseDown={event => event.preventDefault()}>
+                                {item.name}
+                          </div>
              })}
         </ul> }
+            </div>
+        {showModal ?
+        <Modal setShowModal={setShowModal} currentInfo={pokemon}/> : null
+        }
     </div>
 }
 
